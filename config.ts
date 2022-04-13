274cd3e2388cee85f226e942f63aed1d919893a7
Nahiyan Khan <nahiyan.khan@gmail.com>
2022-04-13 11:43:22 -0400

    name change

---
+++ config.ts
@@ -0,0 +1,8 @@
+// notes
+
+// do we need curves for lightness / tone? aim would be to normalize them?
+
+interface Config {
+  name: String,
+
+}
--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,6 @@
 import { HCT } from "@material/material-color-utilities";
 const color = HCT.fromInt(0xff4285f4)
+const test = HCT.from(10, 10, 10)
 
 export function getColor(): {} {
   const toRet = {
